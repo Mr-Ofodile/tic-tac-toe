a0f6cff4cc6b04dcc034b669241bb95b2fccf893
Chigozie Ofodile <ozoofodile@gmail.com>
2024-01-18 17:03:00 +0100

    tic tac toe game

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Toe from './Toe';
+import Circle from './assets/circle.png';
+import cross from './assets/x-cross.jpg';
+function App() {
+  return (
+        <>
+        
+          <Toe/>
+        </>
+  );
+}
+
+export default App;
